efb347c54577665926c628a61b3599c7926a7622
Tom Huynh <tomhuynhsg@gmail.com>
2022-12-10 16:24:02 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Let's introduce!");
+
+        Tom tom = new Tom();
+        System.out.println(tom);
+
+        // Please add your class and create
+        // a new object to print the object like my example
+        // Your turn!
+
+
+    }
+}
---
+++ src/Tom.java
@@ -0,0 +1,10 @@
+// Please create your own class file like my example below
+public class Tom {
+    private String name = "Tom";
+    private String hobby = "basketball";
+
+    @Override
+    public String toString() {
+        return "My name is " + name + " and my hobby is " + hobby;
+    }
+}
