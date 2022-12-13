0e53e8bc6db5fdda23be9dddf55adf89bcd31716
Nhat Tran <95455356+nhattranb@users.noreply.github.com>
2022-12-13 17:15:08 +0700

    Add files via upload
    Nhat Tran adding his Java file...

---
+++ src/NhatTran.java
@@ -0,0 +1,13 @@
+package welcome2023;
+
+public class NhatTran {
+    private String name = "Nhat Tran";
+    private String hobby = "reading news especially Foreign Affairs news";
+
+    @Override
+    public String toString() {
+        return "My name is " + name + '\'' +
+                ", and my hobby is '" + hobby + '\'' +
+                '}';
+    }
+}
