2b37e9900788fb35231e96dc60f7a657b9347378
Ming <102708759+MingK4@users.noreply.github.com>
2022-12-13 16:12:06 +0700

    yes dafunni

---
+++ src/MingK.java
@@ -0,0 +1,13 @@
+public class MingK {
+     private String name = "Ming";
+     private String hobby = "Games";
+
+    @Override
+    public String toString() {
+        return "MingK{" +
+                "name='" + name + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
+
