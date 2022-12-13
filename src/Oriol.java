463b7babc8094bc05b6808f0c1b128ca5289e9f7
Oriol <oriol.moles-teiga@epitech.eu>
2022-12-13 17:58:00 +0700

    Added Oriol's file to the src directoy

---
+++ src/Oriol.java
@@ -0,0 +1,9 @@
+public class Oriol {
+    private String name = "Oriol";
+    private String hobby = "ski";
+
+    @Override
+    public String toString() {
+        return "My name is " + name + " and my hobby is " + hobby;
+    }
+}
