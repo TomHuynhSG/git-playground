261766df7b52c3065276c626a5098a592ddc32f4
an6122003 <an6122003@gmail.com>
2022-12-13 17:59:58 +0700

    -- Added An class, and create an object in main class.

---
+++ src/An.java
@@ -0,0 +1,9 @@
+public class An {
+
+    private String name = "An";
+    private String hobby = "Booking consultation :D";
+
+    public String toString() {
+        return "My name is " + name + " and my hobby is " + hobby;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,8 @@ public class Main {
 
         //:D
 
+        An an = new An();
+        System.out.println(an);
+
     }
 }
