af3e3d1500104930615e36e55428ef353a0aa024
KhoiNguyen1611 <s3926984@rmit.edu.vn>
2022-12-13 18:05:39 +0700

    Add Khoi class to TomHuynh repo

---
+++ src/Khoi.java
@@ -0,0 +1,12 @@
+public class Khoi {
+    private String name = "Khoi";
+    private String hobby = "Music";
+
+    @Override
+    public String toString() {
+        return "Khoi{" +
+                "name='" + name + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
