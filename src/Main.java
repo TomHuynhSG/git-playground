b36d8e1980cde89ba410b29825b312c220c33421
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2022-12-13 15:34:06 +0700

    question 1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         String name, address;
